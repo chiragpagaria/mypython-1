a8822f9903c8fb2bdf7731ee0c569c7ef78df803
aadarsh147 <aadarshs147@gmail.com>
2018-06-12 09:32:40 +0530

    subject

---
+++ abc.py
@@ -0,0 +1,15 @@
+
+xyz=open("C:\\Users\lenovo\Desktop\mypython\\mail.txt", "r")
+
+for line in xyz:
+    
+    if(line.startswith('Subject:')):
+        abc=line.split(':')
+        print(abc[1])
+        #for i in abc:
+        #print(i)
+    
+    #for word in words:
+        #if(word=='Subject:'):
+       #print(line)
+    
